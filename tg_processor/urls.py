df20db791c88ba73b79d1cacdcf9d0ffbf6ec597
Nikita Bogdanov <testik000@ya.ru>
2021-08-02 16:17:14 +0300

    commit

--- tg_processor/urls.py
+++ tg_processor/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from tg_processor import views as tg_views
 
+from django.contrib import admin
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from tg_processor.views import TutorialBotView
+
 urlpatterns = [
 #    path('admin/', admin.site.urls),
-    path('', tg_views.index),
-    path('e656371110e5497bf12bde143b4908ae/', tg_views.bot_input),
+#     path('', tg_views.index),
+    # path('e656371110e5497bf12bde143b4908ae/', tg_views.bot_input),
+    path('e656371110e5497bf12bde143b4908ae/', csrf_exempt(TutorialBotView.as_view())),
 ]
